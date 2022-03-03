8b7953508fe7746bc5c905b71d72ca264c32a154
layeeq07 <98306676+layeeq07@users.noreply.github.com>
2022-03-03 15:07:48 +0530

    Add files via upload

---
+++ Machinefile.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+df = pd.read_csv('newmpg.csv')
+rfc = RandomForestRegressor()
+x = df[['cylinders','displacement','HP','weight','accelartion','modelY','origin']]
+y = df['mgp']
+xtrain,xtest,ytrain,ytest = train_test_split(x,y,test_size=0.2)
+rfc.fit(xtrain,ytrain)
+from fastapi import FastAPI
+app = FastAPI()
+@app.get("/")
+def machine(a:float,b:float,c:float,d:float,e:float,f:float,g:float):
+    pred=rfc.predict([[a,b,c,d,e,f,g]])
+    return(mean_squared_error(pred))
+    
